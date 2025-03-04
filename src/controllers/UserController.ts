d91eabcad588871b07f5a8946728ddce114a8c02
Stéphane BILLOIS <stephane.billois@hotmail.com>
2025-03-04 17:18:49 +0100

    Add login route

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -68,3 +68,47 @@ export const signup = async (req: Request, res: Response) => {
     res.status(500).json({ error: texts.singup_echec[language] || texts.singup_echec.en });
   }
 };
+
+
+export const login = async (req: Request, res: Response) => {
+  const { language } = req.params;
+  const { login, password } = req.body;
+  const regex = /^[a-zA-Z0-9_.\-+]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  const searchField = regex.test(login) ? 'email' : 'login';
+
+  try {
+    // Rechercher l'utilisateur par login ou email
+    const user = await User.findOne({
+      where: {
+        [Op.or]: [{ [searchField]: login }]
+      }
+    });
+
+    if (user) {
+      // Vérifier le mot de passe
+      const currentPassword = await Password.findOne({
+        where: {
+          user_id: user.user_id,
+          is_current: true
+        }
+      });
+
+      if (currentPassword && await argon2.verify(currentPassword.password_hash, password)) {
+        // Générer le token JWT
+        const token = jwt.sign(user.toJSON(), secretKey, { algorithm: 'RS256', expiresIn: '720h' });
+        res.json({
+          response: texts.login_ok[language] || texts.login_ok.en,
+          user,
+          token
+        });
+      } else {
+        res.status(401).json({ error: texts.login_not_found[language] || texts.login_not_found.en });
+      }
+    } else {
+      res.status(401).json({ error: texts.login_not_found[language] || texts.login_not_found.en });
+    }
+  } catch (error) {
+    console.error("Database query error:", error);
+    res.status(500).json({ error: texts.db_internal_error[language] || texts.db_internal_error.en });
+  }
+};
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { signup } from '../controllers/UserController';
+import { login } from '../controllers/UserController';
 
 const router = express.Router();
 
 router.post('/:language/signup', signup);
+router.post('/:language/login', login);
 
 export default router;
