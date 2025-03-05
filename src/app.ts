80698f300ab94f9d04178ef8d479249cc62d46e6
Stéphane BILLOIS <stephane.billois@hotmail.com>
2025-03-05 15:19:10 +0100

    Add Cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import userRoutes from './routes/userRoutes';
 import sequelize from './config/database';
 
 const app = express();
 
+// Configuration de CORS
+app.use(cors({
+  origin: '*', // À restreindre en production (ex: "http://mon-frontend.com")
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 app.use(express.json());
+
 app.use('/api/users', userRoutes);
 
 sequelize.sync().then(() => {
